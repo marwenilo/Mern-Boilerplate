f55f837deeeba144a7030a764e4d604a56bb990b
Marwen Magri <marwen.magri@gomycode.co>
2020-04-22 17:38:38 +0000

    alert ui

--- client/src/components/Alert/Alert.js
+++ client/src/components/Alert/Alert.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import  { connect } from 'react-redux';
-const Alert = ({ alerts}) => alerts !== null && alerts.length > 0 && alerts.map(alert => (
-  <div key ={alert.id} className ={`alert alert-${alert.alertType}`}>
-    { alert.msg }
-  </div>
-));
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import Alert from "@material-ui/lab/Alert";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+const SimpleAlerts = ({ alerts }) => {
+  const classes = useStyles();
+  const alert =
+    alerts !== null &&
+    alerts.length > 0 &&
+    alerts.map((alert) => (
+      <div className={classes.root}>
+        <Alert variant="filled" severity="error" key={alert.id}>
+          {alert.msg}
+        </Alert>
+      </div>
+    ));
+  return alert;
+};
 Alert.propTypes = {
- alerts:PropTypes.array.isRequired,
-}
+  alerts: PropTypes.array.isRequired,
+};
 
-const mapStateToProps = state =>({
-alerts: state.alertReducer
+const mapStateToProps = (state) => ({
+  alerts: state.alertReducer,
 });
-export default connect(mapStateToProps)(Alert);
+export default connect(mapStateToProps)(SimpleAlerts);
